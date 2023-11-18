95f81613320136c806cde69b93762457f4f1a7a3
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-11-18 14:57:45 -0800

    Adds date parsing

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-var APIkey = "";
+var APIkey = "2e17c151d7db1d257660c3301bec1b41";
 var sumbit = $(".search-button");
 var inputLocation = $("#search-input");
 
 // Save search results to local storage
-function searchSave(location) {
-
-}
+function searchSave(location) {}
 
 // Render results on page
-function renderResults(location) {
-    console.log(location)
+function renderResults(results) {
+  console.log(results);
+  var icon = results.list[0].weather[0].icon;
+  var date = dayjs(results.list[0].dt_txt).format("dddd, D MMMM");
+  console.log(date);
 }
 
 // Create event handler for location submission
-sumbit.on("click", function(event) {
-    event.preventDefault();
-    var location = inputLocation.val().trim();
-    // Save to local storage
-    // Fetch request for data
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&cnt=5&units=metric&appid=" + APIkey;
-    fetch(queryURL)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-        })
+sumbit.on("click", function (event) {
+  event.preventDefault();
+  var location = inputLocation.val().trim();
+  // Save to local storage
+  // Fetch request for data
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    location +
+    "&cnt=1&units=metric&appid=" +
+    APIkey;
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // Render results to page
+      renderResults(data);
+    });
 
-    // Render results to page
-    renderResults(location);
-
-    // Render search history
-    
-})
+  // Render search history
+});
 
 // Take user input into query URL
 // Make get request
